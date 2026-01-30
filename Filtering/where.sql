2391d6c31b8e89d1f495a93ddef170805d0e2001
Furkan KALKAN <furkankalkan2023@gmail.com>
2026-01-30 23:02:16 +0300

    WHERE

---
+++ Filtering/where.sql
@@ -0,0 +1,9 @@
+--The WHERE clause is used to filter records.
+
+--It is used to extract only those records that fulfill a specified condition.
+
+--Example
+
+--SELECT * FROM TableName WHERE ColumnName='Value';
+
+SELECT * FROM Customers WHERE Country='Mexico'; 
