d957a09c833f21b95dada2a9a3a12c08f3ff281f
Furkan KALKAN <furkankalkan2023@gmail.com>
2026-02-02 07:54:57 +0300

    AND

---
+++ Filtering/and.sql
@@ -0,0 +1,14 @@
+-- And Operator
+--Where clause can contain one or more and operator to filter your record on sql table. 
+/*
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition1 AND condition2 AND condition3 ...;
+*/
+
+---The AND operator displays a record if all the conditions are TRUE.
+
+---Example
+--Select all customers from Spain that starts with the letter 'F'
+SELECT *FROM Customers WHERE Country = 'Spain' AND CustomerName LIKE 'F%';
+
