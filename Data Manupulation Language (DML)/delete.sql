287b3fab7f6247001ee475c97311f1c791a6b0d6
Furkan KALKAN <furkankalkan2023@gmail.com>
2026-02-03 23:08:45 +0300

    DELETE

---
+++ Data Manupulation Language (DML)/delete.sql
@@ -0,0 +1,20 @@
+---SQL DELETE
+---The DELETE statement delete existing records in a table.
+
+---DELETE FROM table_name WHERE condition;
+
+
+---Example 
+
+DELETE FROM Product WHERE ProductName='Tea';
+
+
+---Delete All Records
+---DELETE FROM table_name;
+DELETE FROM ProductName;
+
+---Delete a Table
+--- You should use DROP TABLE
+
+---Example
+DROP TABLE ProductName;
